d66e2936d1ce0cbac91d0689ee096c8eabe823a9
Matheus Souza <matheu.sk.9@hotmail.com>
2019-12-18 19:01:19 -0300

    Reference all DOM elements that should change

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ class Party {
   }
 }
 
-var party = new Party();
+/*var party = new Party();
 console.log(party.numberOfGuests);
 party.numGuests = 20;
 console.log(party.numberOfGuests);
@@ -60,4 +60,37 @@ party.changeAmount(12, 'numberOfCakeHolders');
 console.log(party.numberOfCakeHolders);
 //party.setAmountToZero('numberOfCakeHolders');
 //console.log(party.numberOfCakeHolders);
-console.log(party.getItemTotal('numberOfCakeHolders'));
+console.log(party.getItemTotal('numberOfCakeHolders'));*/
+
+// DOM references
+const option10 = document.querySelector('#option-10');
+const option15 = document.querySelector('#option-15');
+const option20 = document.querySelector('#option-20');
+
+const cakeCloseBtn = document.querySelector('#cake-holder .close-btn');
+const cakeMinusBtn = document.querySelector('#cake-holder .minus');
+const cakeQtyDisplay = document.querySelector('#cake-holder .item-qty');
+const cakePlusBtn = document.querySelector('#cake-holder .plus');
+const cakeTotalSum = document.querySelector(
+  '#cake-holder .item-total-sum span'
+);
+
+const brigadeiroCloseBtn = document.querySelector('#brigadeiro .close-btn');
+const brigadeiroMinusBtn = document.querySelector('#brigadeiro .minus');
+const brigadeiroQtyDisplay = document.querySelector('#brigadeiro .item-qty');
+const brigadeiroPlusBtn = document.querySelector('#brigadeiro .plus');
+const brigadeiroTotalSum = document.querySelector(
+  '#brigadeiro .item-total-sum span'
+);
+
+const giftBoxCloseBtn = document.querySelector('#gift-box .close-btn');
+const giftBoxMinusBtn = document.querySelector('#gift-box .minus');
+const giftBoxQtyDisplay = document.querySelector('#gift-box .item-qty');
+const giftBoxPlusBtn = document.querySelector('#gift-box .plus');
+const giftBoxTotalSum = document.querySelector(
+  '#gift-box .item-total-sum span'
+);
+
+const totalSum = document.querySelector(
+  '.total-container .total-sum-container span'
+);
